769a505defe66f83624cf5b3e94cbc3e818f632d
Arnav-ops329 <arnavmaitrey44@gmail.com>
2026-05-14 12:03:55 +0530

    Start working with main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is the main file that contains the overall logic
+
+from questions import questions  # To access the questions from a different file
+
+print("===== WELCOME TO THE QUIZ GAME =====")
+input()
+print(
+    "Some questions would be displayed and you have to answer them, based on the answers you would be granted with points."
+)
+input()
