ea6bcb741ca03b31b4e3f0957d7f2ddd00808739
Arnav-ops329 <arnavmaitrey44@gmail.com>
2026-05-14 12:16:06 +0530

    Created a list of dictionary for questions

--- questions.py
+++ questions.py
@@ -1,9 +1,29 @@
 # This file contains questions separately to maintain good readability.
 
-questions = {
-    "When was Java developed": "1995",
-    "Who developed Java": "James Gosling",
-    "Where was Java developed": "Sun Microsystems",
-    "Which company owns java now": "Oracle",
-    "Java is platform dependent or independent language": "Platform Indepedent",
-}
+questions = [
+    {
+        "Question": "When was Java developed ?",
+        "Options": ["1991", "1885", "1995", "2000"],
+        "Answer": "1995",
+    },
+    {
+        "Question": "Who developed Java ?",
+        "Options": [
+            "Guiddo Van Rossum",
+            "James Gosling",
+            "Dennis Ritchie",
+            "Linus Torvalds",
+        ],
+        "Answer": "James Gosling",
+    },
+    {
+        "Question": "Where was Java developed ?",
+        "Options": ["Oracle", "Microsoft", "Sun Microsystems", "Red Hat"],
+        "Answer": "Sun Microsystems",
+    },
+    {
+        "Question": "Who owns Java now ?",
+        "Options:": ["Google", "Sun Microsystems", "Oracle", "BlackRock"],
+        "Answer": "Oracle",
+    },
+]
