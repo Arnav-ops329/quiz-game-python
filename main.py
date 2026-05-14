a345ed08bdccc2edb75bad19ec255c8df2533feb
Arnav-ops329 <arnavmaitrey44@gmail.com>
2026-05-14 12:36:33 +0530

    Add the checking system to check the answer and print the result

--- main.py
+++ main.py
@@ -10,8 +10,18 @@ print(
 input()
 
 score = 0  # To set the score of the user to zero
-for q in questions:
+for q in questions:  # To iterate over the questions
     print("\n" + q["Question"])
 
-    for i, options in enumerate(q["Options"], start=1):
+    for i, options in enumerate(q["Options"], start=1):  # To iterate over the options
         print(f"{i}. {options}")
+    user_answer = input("Enter Your Answer here: ")
+    selected_option = q["Options"][int(user_answer) - 1]
+    if selected_option == q["Answer"]:
+        print("Correct!")
+        score += 1
+    elif selected_option != q["Answer"]:
+        print("Incorrect!")
+    else:
+        print("Enter a valic answer!")
+print(f"FINAL SCORE: {score}")
