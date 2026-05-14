de5dec8a1bf6655836632eb7dd20b7c454c169a9
Arnav-ops329 <arnavmaitrey44@gmail.com>
2026-05-14 11:59:01 +0530

    Add questions related to Java

---
+++ questions.py
@@ -0,0 +1,9 @@
+# This file contains questions separately to maintain good readability.
+
+questions = {
+    "When was Java developed": "1995",
+    "Who developed Java": "James Gosling",
+    "Where was Java developed": "Sun Microsystems",
+    "Which company owns java now": "Oracle",
+    "Java is platform dependent or independent language": "Platform Indepedent",
+}
